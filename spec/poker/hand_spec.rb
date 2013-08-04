32f0879f690b24bd315b244329f4913c9de11f9c
itzki <itzki.h@gmail.com>
2013-08-04 17:43:38 +0900

    Fix spec for straight

--- spec/poker/hand_spec.rb
+++ spec/poker/hand_spec.rb
@@ -1,21 +1,22 @@
 $:.unshift(File.expand_path('../../', __FILE__))
 require 'spec_helper'
 
-cases = {
-  'straight flush' => 'As Ks Qs Js Ts',
-  'four of a kind' => '7s 7h 7d 7c As',
-  'full house' => 'Ts Th Td 7c 7d',
-  'flush' => 'Ad 4d 5d Jd Kd',
-  'straight' => '2s 3h 4s 5d 6c',
-  'three of a kind' => '9s 9h 9d Ts 3s',
-  'two pair' => 'Ts Th 2c 2h 5d',
-  'one pair' => '2s 2d 5c 6d 9c',
-  'high cards' => 'Ah Jc 5d 4s 9c',
+CASES = {
+  'As Ks Qs Js Ts' => 'straight flush',
+  '7s 7h 7d 7c As' => 'four of a kind',
+  'Ts Th Td 7c 7d' => 'full house',
+  'Ad 4d 5d Jd Kd' => 'flush',
+  'Ac 2s 3h 4s 5d' => 'straight',
+  'Ts Jh Qs Kd Ac' => 'straight',
+  '9s 9h 9d Ts 3s' => 'three of a kind',
+  'Ts Th 2c 2h 5d' => 'two pair',
+  '2s 2d 5c 6d 9c' => 'one pair',
+  'Ah Jc 5d 4s 9c' => 'high cards',
 }
 
 describe Poker::Hand do
-  describe '#detect' do
-    cases.each do |hand_name, card_args|
+  describe '#judge' do
+    CASES.each do |card_args, hand_name|
       describe %[when given cards "#{card_args}"] do
         let(:hand){ Poker::Hand.new(*card_args.split(' ')) }
         subject{ hand.judge }
