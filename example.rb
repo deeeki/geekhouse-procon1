30969b0ed5b3b0fd33d26a03c17fa0d59f37bc28
itzki <itzki.h@gmail.com>
2013-08-04 18:08:20 +0900

    Modify example

--- example.rb
+++ example.rb
@@ -2,19 +2,15 @@ $:.unshift(File.expand_path('../lib', __FILE__))
 require 'poker'
 require 'pp'
 
-hand = Poker::Hand.new 'Ac', 'Ah', 'Kh', 'Qc', '2d'
+card = Poker::Card.new 'As'
+pp card
 
-# 手札のハイカードになりうる数値
-pp hand.high_number
-# 手札の数値の配列
-pp hand.numbers
-# 手札の絵柄の配列
-pp hand.suits
+hand = Poker::Hand.new 'Ac', 'Ah', 'Kh', 'Qc', '2d'
+# 手札
+pp hand.cards
 # 手札に数値がいくつあるかのハッシュ
-pp hand.number_stats
+pp hand.instance_variable_get('@number_stats')
 # 手札に絵柄がいくつあるかのハッシュ
-pp hand.suit_stats
-# 手札からストレートを作るための数値の配列
-pp hand.__send__(:straight_numbers)
+pp hand.instance_variable_get('@suit_stats')
 # 判定
 pp hand.judge
